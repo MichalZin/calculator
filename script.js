76443357655d45a54c40c0bc847f4b5b912a8258
MichalZin <124308169+MichalZin@users.noreply.github.com.>
2024-06-26 13:32:10 +0200

    Creation of calculation function

--- script.js
+++ script.js
@@ -1,23 +1,73 @@
 const display = document.getElementsByClassName("display")[0];
-const btt = document.querySelectorAll("button");
+const buttons = document.querySelectorAll(".btt");
+const clear = document.querySelector("clear")
+const functionKey = document.querySelectorAll("funk")
 
 
 
-
-btt.forEach((button) => {
-    button.addEventListener('click', () => {
+buttons.forEach((btt) => {
+    btt.addEventListener('click', () => {
         const paragraph = document.createElement('p');
-        paragraph.innerText = `${button.value}`
-        display.appendChild(paragraph)
-    });
+
+        if (btt.value !== 'clear' && btt.value !== 'result') {
+
+            
+            paragraph.innerText = `${btt.value}`
+            display.appendChild(paragraph)
+        }
+        
+        if (btt.value === 'clear') {
+            while(display.firstChild) {
+                display.removeChild(display.firstChild)
+            }
+        }
+
+        if (btt.value === 'result') {
+            let result = '';
+            for (let i = 0; i < display.childNodes.length; i++) {
+                result += display.childNodes[i].textContent;
+            }
+        
+            const numbers = result.split(' ');
+            const operatorKey = numbers[1];
+        
+            if (/^[+\-*/]$/.test(operatorKey)) {
+                const firstChild = parseFloat(numbers[0]);
+                const lastChild = parseFloat(numbers[2]);
+        
+                const displayResults = () => {
+                    if (operatorKey === '+') {
+                        const sum = firstChild + lastChild;
+                        console.log(sum);
+                    } else if (operatorKey === '-') {
+                        const diff = firstChild - lastChild;
+                        console.log(diff);
+                    } else if (operatorKey === '*') {
+                        const multip = firstChild * lastChild;
+                        console.log(multip);
+                    } else if (operatorKey === '/') {
+                        const diff = firstChild / lastChild;
+                        console.log(diff);
+                    }
+
+                };
+                displayResults()
+            }
+            
+        }
+            });
+        
+        });
+    
+        
+    
+
+
     /*
     const firstChild = display.firstChild;
     const lastChild = display.lastChild;
 
     const firstChildValue = parseFloat(firstChild.textContent);
     const lastChildValue = parseFloat(lastChild.textContent);
-*/
+*/   
 
-    
-
-});
